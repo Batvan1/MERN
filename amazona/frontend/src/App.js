cecbfd797e575e5030fe0ece9f5d0c99ffb79d47
batvan1 <krayyark8@gmail.com>
2023-03-06 16:59:10 +0300

    two push

--- amazona/frontend/src/App.js
+++ amazona/frontend/src/App.js
@@ -1,3 +1,5 @@
+import data from './data.js'
+
 
 function App() {
   return (
@@ -6,7 +8,33 @@ function App() {
         <a href="/">amazona</a>
       </header>
       <main>
-        list product
+
+        <h1>Featured Products</h1>
+
+        <div className="products">
+
+          {data.products.map(product => (
+            <div className="product" key={product.slug}>
+
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name}></img>
+              </a>
+
+              <div className="product-info">
+
+                <a href={`/product/${product.slug}`}>
+                  <p> {product.name}</p>
+                </a>
+                
+                <p> <strong>{product.price} </strong></p>
+                <button>Add to cart</button>
+
+              </div>
+
+            </div>
+
+          ))}
+        </div>
       </main>
     </div>
   );
---
+++ amazona/frontend/src/data.js
@@ -0,0 +1,54 @@
+const data = {
+    products:[
+        {
+            name: "Nike Slim shirt",
+            slug: "nike-slim-shirtt",
+            category: "shirts",
+            image: "/images/p1.jpg",
+            price: 200,
+            countInStock: 10,
+            brand: "nike",
+            rating: 4.5,
+            numRevievs: 10,
+            description: "high quality shirt"
+        },
+        {
+            name: "Nike Slim shirt",
+            slug: "adidas-wow",
+            category: "shirts",
+            image: "/images/p2.jpg",
+            price: 300,
+            countInStock: 10,
+            brand: "nike",
+            rating: 4.5,
+            numRevievs: 10,
+            description: "high quality shirt"
+        },
+        {
+            name: "Nike Slim shirt",
+            slug: "puma-my-got",
+            category: "shirts",
+            image: "/images/p3.jpg",
+            price: 188,
+            countInStock: 10,
+            brand: "nike",
+            rating: 4.5,
+            numRevievs: 10,
+            description: "high quality shirt"
+        },
+        {
+            name: "Nike Slim shirt",
+            slug: "beymen",
+            category: "shirts",
+            image: "/images/p4.jpg",
+            price: 700,
+            countInStock: 10,
+            brand: "nike",
+            rating: 4.5,
+            numRevievs: 10,
+            description: "high quality shirt"
+        }
+    ]
+}
+
+export default data
