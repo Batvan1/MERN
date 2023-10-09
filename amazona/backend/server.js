5803c72a3ef110903a36acd08fe0717546740980
batvan1 <krayyark8@gmail.com>
2023-10-09 22:12:09 +0300

    publish

--- amazona/backend/server.js
+++ amazona/backend/server.js
@@ -22,10 +22,10 @@ app.use('/api/communication', communicationRouter)
 
 
 const __dirname = path.resolve() // incele
-app.use(express.static(path.join(__dirname, '../frontend/build'))) // incele
+app.use(express.static(path.join(__dirname, '/frontend/build'))) // incele
 
 app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname,'../frontend/build/index.html')) // incele
+    res.sendFile(path.join(__dirname,'/frontend/build/index.html')) // incele
 })
 
 
