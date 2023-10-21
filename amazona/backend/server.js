c690f133ad4a738f8e4c431908b57894802b4930
Batuhan Öztüfekçi <krayyark8@gmail.com>
2023-10-21 17:21:28 +0300

    server js buid index n0

--- amazona/backend/server.js
+++ amazona/backend/server.js
@@ -28,12 +28,7 @@ app.use('/api/orders', orderRouter)
 app.use('/api/communication', communicationRouter)
 
 
-const __dirname = path.resolve() // incele
-app.use(express.static(path.join(__dirname, '/frontend/build'))) // incele
 
-app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname,'/frontend/build/index.html')) // incele
-})
 
 
 app.use((err, req, res, next)=>{
